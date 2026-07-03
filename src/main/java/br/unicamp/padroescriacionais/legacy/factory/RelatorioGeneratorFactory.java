2a634126f42b5b8a55163987ba552ec431f7e6d7
Mauricio-MSantos <m241949@dac.unicamp.br>
2026-07-03 15:41:33 -0300

    Add RelatorioGeneratorFactory abstract class

---
+++ src/main/java/br/unicamp/padroescriacionais/legacy/factory/RelatorioGeneratorFactory.java
@@ -0,0 +1,8 @@
+package br.unicamp.padroescriacionais.legacy.factory;
+
+import br.unicamp.padroescriacionais.legacy.generator.RelatorioGenerator;
+
+public abstract class RelatorioGeneratorFactory {
+
+    public abstract RelatorioGenerator criarGenerator();
+}
